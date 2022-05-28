2644f605789988aaaf5182724906678d9e6c4106
mahersalhani <maheryos5@gmail.com>
2022-05-28 23:59:41 +0300

    add structure

---
+++ src/components/home/NavBarLogIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBarLogIn = () => {
+  return <div>NavBarLogIn</div>;
+};
+
+export default NavBarLogIn;
---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBarLogIn from "./../../components/home/NavBarLogIn";
+import Slider from "./../../components/home/Slider";
+
+const HomePage = () => {
+  return (
+    <div className="font">
+      <NavBarLogIn></NavBarLogIn>
+      <Slider></Slider>
+    </div>
+  );
+};
+
+export default HomePage;
